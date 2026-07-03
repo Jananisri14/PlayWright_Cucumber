c4202ae5c5b573cedd5524a7e45f037764886b96
Jananisri14 <jananisathishkumar380@gmail.com>
2026-07-03 09:53:08 +0530

    Update hooks.ts

--- PlayWright_Cucumber/src/test/hooks/hooks.ts
+++ PlayWright_Cucumber/src/test/hooks/hooks.ts
@@ -1,20 +1,36 @@
-import { Before,After, Status } from "@cucumber/cucumber";
+import { Before, After, Status, setDefaultTimeout } from "@cucumber/cucumber";
 import { chromium } from "@playwright/test";
 import { CustomWorld } from "./world";
-Before(async function(this:CustomWorld){
-    this.browser=await chromium.launch({headless:true});
-    this.context=await this.browser.newContext();
-    this.page=await this.context.newPage();
-    
-})
-After(async function ({pickle,result}){
-    console.log(result?.status);
-    if(result?.status==Status.FAILED){
-        const img=await this.page.screenshot({path:`./test-results/screenshots/${pickle.name}.png`,type:"png"});
-        await this.attach(img,"img/png");
-    }
-    await this.page.close();
-    await this.context.close();
 
-    
-})
+setDefaultTimeout(60000);
+
+Before(async function (this: CustomWorld) {
+
+    this.browser = await chromium.launch({
+        headless: true
+    });
+
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
+
+});
+
+After(async function ({ pickle, result }) {
+
+    console.log(result?.status);
+
+    if (result?.status === Status.FAILED && this.page) {
+
+        const img = await this.page.screenshot({
+            path: `./test-results/screenshots/${pickle.name}.png`,
+            type: "png"
+        });
+
+        await this.attach(img, "image/png");
+    }
+
+    await this.page?.close();
+    await this.context?.close();
+    await this.browser?.close();
+
+});
